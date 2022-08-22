3dbb6e307049021ce03dbe69fcf7af7fcfca11e2
Rahul Shardha <rahul.shardha@gmail.com>
2022-08-22 08:19:49 -0400

    Linting

--- app/api/views.py
+++ app/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import json
 
 from .models import Patient
 from .serializers import PatientSerializer
